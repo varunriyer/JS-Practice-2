1108135e03d65703b412a5ba176a1a76f67144cf
varunriyer <vs7835@srmist.edu.in>
2025-03-28 17:22:09 +0530

    Modified to show cost price and selling price

--- JavaScript_DSA_Remaining/array_test.js
+++ JavaScript_DSA_Remaining/array_test.js
@@ -1,10 +1,13 @@
 var arr = [13, 2, 7, 10, 12, 16, 4, 3, 17, 1];
 var sum = 0;
+var sp, cp;
 if (arr.length > 1) {
     for (j = (arr.length - 1); j > 0; j--) {
         for (i = 0; i < j; i++) {
             if ((arr[j] - arr[i] > sum)) {
                 sum = arr[j] - arr[i];
+                sp = arr[j];
+                cp = arr[i]
             }
         }
     }
@@ -12,4 +15,5 @@ if (arr.length > 1) {
 else {
     console.log("Empty Array or only 1 element");
 }
-console.log(`The maximum profit is ${sum}`);
+console.log(`The maximum profit is ${sum} `);
+console.log(`The Cost Price is = ${cp} and Selling Price is = ${sp}`);
