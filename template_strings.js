490cf9643b2b8ac2172f3d532d4024c5b17240c2
varunriyer <vs7835@srmist.edu.in>
2025-03-29 11:40:39 +0530

    Mast

---
+++ template_strings.js
@@ -0,0 +1,20 @@
+var text = `Hello World!`;
+console.log(text);
+
+console.log(`We can now use "quotes" inside a 'string'!!`);
+
+console.log(
+    `It 
+also 
+allows
+multiline
+strings`);
+
+text = "string";
+console.log(`One of the main use cases of template strings is ${text} interpolation`);
+
+cp = 10;
+sp = 17;
+console.log(`The profit is ${sp - cp}`); //it also allows expressions in the string 
+
+
