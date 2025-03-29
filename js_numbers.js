7567627cc7d6ea05c141dfd3e258d09b072ff1b1
varunriyer <vs7835@srmist.edu.in>
2025-03-29 11:56:16 +0530

    Understood different number properties in JavaScript

---
+++ js_numbers.js
@@ -0,0 +1,23 @@
+let num1 = 3;
+let num2 = 3.332138;
+console.log(num1);
+console.log(num2);
+
+let num3 = 123e5;
+let num4 = 123e-5;
+console.log(num3);
+console.log(num4);
+
+let num5 = 999999999999999;
+let num6 = 9999999999999999;
+console.log(num5);
+console.log(num6); //precision available upto 15 digits (maximum number of decimals is 17)
+
+let num7 = 0.2 + 0.1;
+console.log(num7); //gives 0.3000000000004
+
+let num8 = (0.2 * 10 + 0.1 * 10) / 10; // gives 0.3
+console.log(num8);
+
+
+
