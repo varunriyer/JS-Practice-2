7fbf83de29300e7f3920bef61d6e02563d9df9a8
varunriyer <vs7835@srmist.edu.in>
2025-03-29 10:35:44 +0530

    Practiced various string functions to improve knowledge of strings

--- JavaScript_DSA_Remaining/mastering_strings.js
+++ JavaScript_DSA_Remaining/mastering_strings.js
@@ -31,3 +31,60 @@ console.log(text3.trim() + " " + " haha"); //trim removes whitespace from both s
 console.log(text3.trimStart() + " " + "haha"); //trimStart removes whitespace only at the start of the string
 console.log(text3.trimEnd() + " " + "haha"); //similar to above
 
+//String Padding 
+var text = "5";
+var padded = text.padStart(4, "0"); //pads 0 until it reaches the length 4
+console.log(padded); // prints "0005"
+
+var padded = text.padStart(4, "x");
+console.log(padded); //prints "xxx5";
+
+var num = 7;
+var text = num.toString(); //padStart() is a string function so have to convert number to string
+console.log(text.padStart(6, "o"));
+
+var text = "jack";
+console.log(text.padEnd(7, "x")); //similar to padStart() but till it reaches the end
+
+//Replacing String 
+
+var text = "Today is a good day!";
+console.log(text.replace("good", "bad")); //prints 'Today is a bad day!'
+console.log(text.replace("Good", "bad")); //prints 'Today is a good day!' (.replace() is case-sensitive)
+
+console.log(text.replace(/GOOD/i, "bad")); //you can use /i flag to make it insensitive 
+
+console.log(text.replace(/day/g, "night")); //you can use /g flag to make it global match 
+
+var text = "Cats are animals. cats are pets";
+console.log(text.replaceAll(/Cats/gi, "Dogs"));
+
+//Converting string to an array 
+var text = "1,2,3,4,5"
+console.log(text.split(","));
+console.log(text[2]);
+
+var text = "A E I O U"
+console.log(text.split(" "));
+
+var text = "Hello";
+console.log(text.split(""));
+
+//String Search 
+var text = "Today is 1 fine day!"
+console.log(text.indexOf("is")); //returns first index of the string (Output: 6)
+console.log(text.indexOf("bad")); // returns -1 if not found 
+
+console.log(text.indexOf("day")); //returns first index (2)
+console.log(text.lastIndexOf("day")); //returns last index (16)
+
+console.log(text.indexOf("day", 10)); //we can provide the starting point from where it has to seach the index as well 
+
+console.log(text.indexOf(/Day/i)); // returns -1 (indecOf() cannot be used to search regular expressions)
+console.log(text.search(/DAY/i)); // returns 2 and hence is used with regular expressions [but search cannot take the second value parameter to specify from where to start searching]
+
+var text = "Rain in SPAIN stays mainly in the plain";
+console.log(text.match(/ain/gi));
+
+
+
