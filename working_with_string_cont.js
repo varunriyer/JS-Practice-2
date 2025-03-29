1e088be00e8fac73655ad17cbfd346626e19c832
varunriyer <vs7835@srmist.edu.in>
2025-03-29 11:06:10 +0530

    Worked with startsWith() & endsWith() functions

--- working_with_string_cont.js
+++ working_with_string_cont.js
@@ -4,3 +4,15 @@ var text = "Today is a Saturday!";
 console.log(text.includes("day"));
 console.log(text.includes("DaY"));
 console.log(text.includes("day", 10));
+
+//String startWith()
+
+var text = "Hello World Hello Again!";
+console.log(text.startsWith("hello")); //false
+console.log(text.startsWith("Hello")); //true
+console.log(text.startsWith("Hello", 7)); //false
+console.log(text.startsWith("Hello", 12)); //true
+
+//String endsWith()
+
+console.log(text.endsWith("Again!"));
