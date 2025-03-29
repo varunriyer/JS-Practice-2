58049aa80483e8f7e09232ad06e8711b00a598ac
varunriyer <vs7835@srmist.edu.in>
2025-03-29 15:13:18 +0530

    creating an array using new keyword and Array() --> not required but good to know

--- arrays_basics_2_advanced.js
+++ arrays_basics_2_advanced.js
@@ -19,3 +19,8 @@ arr[0] = 3;
 arr[1] = 4;
 arr[2] = 5;
 console.log(arr);
+
+// You can use new keyword with Array() but it is not required and it is better to easily create it using the above methods
+arr = new Array(1, 2, 3);
+console.log(arr);
+
