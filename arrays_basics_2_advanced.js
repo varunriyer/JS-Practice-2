31a1089d5938c02f54c5919e71ad994861e423c8
varunriyer <vs7835@srmist.edu.in>
2025-03-29 14:41:53 +0530

    Explored other ways you can create an array

--- arrays_basics_2_advanced.js
+++ arrays_basics_2_advanced.js
@@ -4,3 +4,18 @@ arr = ["red", "blue", "green"];
 console.log(arr);
 arr = [8, 9, 10];
 console.log(arr);
+
+//Other Ways you can create an array 
+
+arr = [
+    "Car",
+    "Bike",
+    "Bus"
+];
+console.log(arr);
+
+arr = [];
+arr[0] = 3;
+arr[1] = 4;
+arr[2] = 5;
+console.log(arr);
