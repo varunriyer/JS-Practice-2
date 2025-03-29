49d48057fc1818d2c49cd711dc44f45b11f9507b
varunriyer <vs7835@srmist.edu.in>
2025-03-29 14:37:13 +0530

    Understood and worked with different various number properties in JavaScript such as EPSILON, MAX_VALUE, MIN_VALUE, MAX_SAFE_INTEGER, MIN_SAFE_INTEGER, Positive Infinity and Negative Infinity

---
+++ js_number_properties.js
@@ -0,0 +1,18 @@
+//Number Properties can only be used on Objects and not on Variables
+
+//EPSILON 
+console.log(Number.EPSILON); // difference between the smallest floating point number greater than 1 and 1. (very rare use cases)
+
+//MAX_VALUE & MIN_VALUE
+console.log(Number.MAX_VALUE); //Self Explanatory
+console.log(Number.MIN_VALUE); //Self Explanatory
+
+//MAX_SAFE_INTEGER & MIN_SAFE_INTEGER 
+console.log(Number.MAX_SAFE_INTEGER); //Self Explanatory
+console.log(Number.MIN_SAFE_INTEGER);
+
+//Positive & Negative Infinity 
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
+console.log(`${1 / 0} & ${-1 / 0}`);
+
