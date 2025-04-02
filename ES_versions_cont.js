6580ed37d5e19c8f7af66bbf2970e7c28ecdafe0
varunriyer <vs7835@srmist.edu.in>
2025-04-02 14:21:20 +0530

    Add example for Array.keys() method in ES_versions_cont.js

--- ES_versions_cont.js
+++ ES_versions_cont.js
@@ -12,3 +12,15 @@
 
 console.log(Array.from("ABCDEFG"));
 console.log(Array.from(`123,ere,test`));
+
+//Array keys() - returns an array iterator object with the keys of an array
+{
+    let arr = ["Apple", "Banana", "Pineapple"];
+    let keys = arr.keys();
+
+    let txt = "";
+    for (let x of keys) {
+        txt += x + " ";
+    }
+    console.log(txt);
+}
