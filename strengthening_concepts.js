d3920f10373b88cd90c5ff077846f98a73df4f7f
varunriyer <vs7835@srmist.edu.in>
2025-04-02 16:59:10 +0530

    Added notes forEach() method explanation and add filter() method example(filter out all the odd numbers in an array) in strengthening_concepts.js

--- strengthening_concepts.js
+++ strengthening_concepts.js
@@ -1,4 +1,4 @@
-//forEach() method 
+//forEach() method - forEach() method basically gives the values based on the function. It does not change the original array.
 //Find the square of all numbers present in array
 {
     let arr = [1, 2, 3, 4, 5];
@@ -9,4 +9,11 @@
 {
     let str = ["Hello", "World"];
     str.forEach(x => console.log(x + "!"));
+}
+
+//filter() method - filter() method provides a new array based on the elements satisfying the condition provided. It does not change the original array
+//Filter out all the odd numbers from an array 
+{
+    let nums = [1, 2, 3, 4, 5, 6, 7];
+    console.log(nums.filter(num => num % 2 != 0));
 }
