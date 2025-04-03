601177e10c23499f9659afae253013f19ae94355
varunriyer <vs7835@srmist.edu.in>
2025-04-03 10:09:45 +0530

    Added switch-case implementation to check if a color is part of RGB in strengthening_concepts.js

--- strengthening_concepts.js
+++ strengthening_concepts.js
@@ -72,3 +72,23 @@
     console.log(greet("Jeremy"));
     console.log(typeof (greet)); //returns function 
 }
+
+//implementing switch-case as have not used it with javascript
+{
+    //Check if a color is part of RGB or not
+    let color = "blue";
+    switch (color) {
+        case "red":
+            console.log(`${color} is part of RGB`);
+            break;
+        case "green":
+            console.log(`${color} is part of RGB`);
+            break;
+        case "blue":
+            console.log(`${color} is part of RGB`);
+            break;
+        default:
+            console.log(`${color} is not part of RGB`);
+            break;
+    }
+}
