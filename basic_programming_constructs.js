984d1aab1c465dfdbea7380444d2fc59d49bd77b
varunriyer <vs7835@srmist.edu.in>
2025-04-03 15:21:34 +0530

    Added example demonstrating string manipulation methods including conversion to uppercase, trimming, and checking for substring presence in basic_programming_constructs.js

--- basic_programming_constructs.js
+++ basic_programming_constructs.js
@@ -269,3 +269,13 @@ Product: 42 */
     console.log(calc.multiply(6, 7));
     console.log(calc.divide(3, 3));
 }
+
+//Methods of Primitives 
+//Input a string and convert it to uppercase, check if it includes the word "Javascript" and also trim any extra spaces from the start to end
+{
+    let str = "I love JavaScript!"
+    let new_str = str.toUpperCase();
+    new_str = new_str.trim();
+    console.log(new_str.toLowerCase().includes('javascript'));
+    console.log(new_str);
+}
