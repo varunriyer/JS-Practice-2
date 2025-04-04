fb83c3612ab57dd94f2e59ac2a47d9e5283afff5
varunriyer <vs7835@srmist.edu.in>
2025-04-04 10:39:40 +0530

    Add function to search for a substring in a string and return it's index

--- Strings_practice.js
+++ Strings_practice.js
@@ -13,3 +13,19 @@ Returns both conversions as an object.*/
     };
     console.log(convertCase("Hello World!"));
 }
+
+/* Question 2: String Search
+Write a function searchSubstring that:
+Takes a string and a substring as inputs.
+Checks if the substring exists in the string using .includes().
+Finds the starting index of the substring using .indexOf().
+Returns an object with the results.*/
+{
+    function searchSubstring(str, search_str) {
+        return {
+            exists: str.includes(search_str),
+            index: str.indexOf(search_str)
+        }
+    };
+    console.log(searchSubstring("JavaScript is awesome", "awesome"));
+}
