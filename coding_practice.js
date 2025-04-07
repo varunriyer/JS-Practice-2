71443319d889923c65c99d267bdb625ec62534fa
varunriyer <vs7835@srmist.edu.in>
2025-04-07 14:34:03 +0530

    Add function to check balanced brackets in a string

---
+++ coding_practice.js
@@ -0,0 +1,17 @@
+function areBracketsBalanced(str) {
+    let cleaned_arr = str.trim().split("");
+    let check_arr = [];
+    for (let x of cleaned_arr) {
+        if (x === '[' || x === '{' || x === '(') {
+            check_arr.push(x);
+        }
+        else if (x === ']' || x === '}' || x === ')') {
+            let last = check_arr.pop();
+            if ((x === ']' && last !== '[') || (x === '}' && last !== '{') || (x === ')' && last !== '(')) {
+                return false;
+            }
+        }
+    }
+    return check_arr.length === 0;
+}
+console.log(areBracketsBalanced("[a{b(c)d}e]a"));
